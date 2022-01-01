a9a56d52346b9d07628f978e357bf03e60c2f351
BatcheDaka <dakata@gmail.com>
2022-01-01 20:34:11 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <h1>Lets build Faceboook Surat Tefter</h1>
+    </div>
+  );
+}
+ 
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3L0pW8i5HpP7hLGkBzEp8Ya3vpzW9cng",
+    authDomain: "surat-tefter.firebaseapp.com",
+    projectId: "surat-tefter",
+    storageBucket: "surat-tefter.appspot.com",
+    messagingSenderId: "848208034073",
+    appId: "1:848208034073:web:684dc3e8b5bf85ea5c6d88",
+    measurementId: "G-JD9D16RFR2"
+  };

5719049367f08e34871e77b779d2c578be63772c
BatcheDaka <dakata@gmail.com>
2022-01-01 21:06:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
     <h1>Lets build Faceboook Surat Tefter</h1>
+    <Header/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+      <img src="https://180dc.org/wp-content/uploads/2014/04/facebook-logo-facebook-logo-9.png" alt="facebook"/>
+      </div>
+      <div className="header__middle"></div>
+      <div className="header__right"></div>
+      
+    </div>
+  )
+}

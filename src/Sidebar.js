da0147c811d86d063ff5f7720c4c5e61808be737
BatcheDaka <dakata@gmail.com>
2022-01-03 16:48:40 +0200

    updates

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,27 @@
+import ExpandMore from '@mui/icons-material/ExpandMore';
 import React from 'react';
 import './Sidebar.css';
 import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import EmojiFlagsIcon from "@mui/icons-material/EmojiFlags";
+import PeopleIcon from "@mui/icons-material/People"; 
+import ChatIcon from "@mui/icons-material/Chat"; 
+import StorefrontIcon from "@mui/icons-material/Storefront"; 
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary"; 
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow srce={user.photoURL} title='Pages'/>
-      <SidebarRow title='Friends'/>
-      <SidebarRow />
-      <SidebarRow />
-      <SidebarRow />
+      <SidebarRow src='{user.photoURL}' title="Batche Daka"/>
+      <SidebarRow
+      Icon={LocalHospitalIcon}
+       title="COVID-19 Information Center"/>
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
+      <SidebarRow Icon={PeopleIcon} title="Friends"/>
+      <SidebarRow Icon={ChatIcon} title="Messenger"/>
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace"/>
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos"/>
+      <SidebarRow Icon={ExpandMore} title="Marketplace"/>
     </div>
   )
 }
---
+++ src/Story.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Story.css'
+
+function Story({image, profileSrc, title}) {
+  return (
+    <div className="story">
+      <Avatar src={profileSrc} />
+    </div>
+  )
+}
+
+export default Story
+  

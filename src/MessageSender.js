01f01f88d8be23ffd556dd429e3fb35aa07b69c0
BatcheDaka <dakata@gmail.com>
2022-01-03 20:39:48 +0200

    update

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./MessageSender.css";
-import {Avatar} from "@mui/material"
+import {Avatar} from "@mui/material";
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 function MessageSender() {
+    const [input, setInput] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    };
   return (
     <div className="messageSender">
       
@@ -10,15 +19,38 @@ function MessageSender() {
        <Avatar />
        <form>
            <input 
+           value={input}
+           onChange={e => setInput(e.target.value)}
            className="messageSender__input"
            placeholder={`What's on your mind?`}
            />
-           <input type="text"/>
+           <input 
+           value={imageUrl}
+           onChange={e => setImageUrl(e.target.value)}
+            placeholder={`image URL (Optional)`}
+           />
+           <button onClick={handleSubmit} type="submit">
+               Hidden submit
+           </button>
        </form>
       </div>
 
       <div className="messageSender__bottom">
+   <div className="messageSender__option">
+       <VideocamIcon style={{color: "red"}} />
+       <h3>Live Video</h3>
 
+   </div>
+   <div className="messageSender__option">
+       <PhotoLibraryIcon style={{color: "green"}} />
+       <h3>Photo/Video</h3>
+
+   </div>
+   <div className="messageSender__option">
+       <InsertEmoticonIcon style={{color: "orange"}} />
+       <h3>Feeling/Activity</h3>
+
+   </div>
       </div>
 
     </div>
